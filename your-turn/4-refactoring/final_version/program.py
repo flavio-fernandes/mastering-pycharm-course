1fa11908c4a13ca66aad14da0cf792531035c666
Flavio Fernandes <flavio@flaviof.com>
2018-11-25 18:08:46 -0500

    refactored

--- your-turn/4-refactoring/final_version/program.py
+++ your-turn/4-refactoring/final_version/program.py
@@ -1,51 +1,34 @@
 import random
-from collections import namedtuple
-from xml.etree import ElementTree
 
-import requests
-
-Episode = namedtuple('Episode', 'title link pubdate show_id')
-episode_data = {}
+from service import get_episode, get_latest_show_id, download_data
 
 
 def main():
-    # SHOW THE HEADER
-    print("Welcome to the talk python info downloader.")
-    print()
+    show_header()
 
-    # DOWNLOAD THE EPISODE DATA
-    url = 'https://talkpython.fm/episodes/rss'
-
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    dom = ElementTree.fromstring(resp.text)
-
-    episode_count = len(dom.findall('channel/item'))
-
-    for idx, item in enumerate(dom.findall('channel/item')):
-        episode = Episode(
-            item.find('title').text,
-            item.find('link').text,
-            item.find('pubDate').text,
-            episode_count - idx - 1
-        )
-        episode_data[episode.show_id] = episode
+    download_data()
 
     # GET LATEST SHOW ID
-    latest_show_id = max(episode_data.keys())
+    latest_show_id = get_latest_show_id()
 
     print("Working with total of {} episodes".format(latest_show_id))
 
-    # DISPLAY RESULTS
-    start = random.randint(90, 110)
-    latest_id = max(episode_data.keys())
-    end = random.randint(130, latest_id + 1)
-
+    end, start = display_reults()
     for show_id in range(start, end):
-        # GET EPISODE
-        info = episode_data.get(show_id)
+        info = get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
+
+
+def display_reults():
+    start = random.randint(90, 110)
+    latest_id = get_latest_show_id()
+    end = random.randint(130, latest_id + 1)
+    return end, start
+
+
+def show_header():
+    print("Welcome to the talk python info downloader.")
+    print()
 
 
 if __name__ == '__main__':
