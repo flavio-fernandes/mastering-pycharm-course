ebc392e66f606d97d0c09a5a25a758b5f0edcf7b
Flavio Fernandes <flavio@flaviof.com>
2019-01-02 14:54:20 -0500

    wip.8_packages

---
+++ demos/projects/8_project_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='calcy',
+    version='0.0.1',
+    packages=['calcy'],
+    url='http://calcy.example.com',
+    license='MIT',
+    author='gute',
+    author_email='fff@flaviof.com',
+    description='This is a calcy'
+)
